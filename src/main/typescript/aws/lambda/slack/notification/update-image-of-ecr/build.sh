515fef7a5a3033e9a7ccb3f8f7b1891daaf2f2f3
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-01-26 23:08:20 +0900

    Fix build shell script

--- src/main/typescript/aws/lambda/slack/notification/update-image-of-ecr/build.sh
+++ src/main/typescript/aws/lambda/slack/notification/update-image-of-ecr/build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 echo remove dir and zip
-rm -rf update-image0of-ecr-dist/
+rm -rf update-image-of-ecr-dist/
 
 echo compile ts
 tsc -p tsconfig.json
 
 echo copy package.json
-cp -f ./package.json ./update-image0of-ecr-dist
+cp -f ./package.json ./update-image-of-ecr-dist
 
 echo install module
-cd update-image0of-ecr-dist
+cd update-image-of-ecr-dist
 npm install
 
 echo zip
-zip -r update-image0of-ecr-dist.zip ./
+zip -r update-image-of-ecr-dist.zip ./
