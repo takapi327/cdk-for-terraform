03ee214bb97bdbf43ae76399ab72fbcb126a3025
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-03-04 23:33:07 +0900

    Fix VPC

--- lib/module/networkLayer/vpc.ts
+++ lib/module/networkLayer/vpc.ts
@@ -3,10 +3,10 @@ import { Vpc } from '../../../.gen/providers/aws';
 
 export namespace VpcModule {
   export function create(scope: Construct): Vpc {
-    return new Vpc(scope, 'vpc-for-cdktf', {
+    return new Vpc(scope, 'cdktf-production', {
       cidrBlock:          '10.0.0.0/16',
       enableDnsHostnames: true,
-      tags:               { ['Name']: 'ECS vpc-for-cdktf' }
+      tags:               { ['Name']: 'cdktf production' }
     });
   }
 }
