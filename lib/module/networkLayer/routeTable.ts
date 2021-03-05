00ffd675e601f5976a4a0fe2befecc86609c4508
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-03-05 23:40:45 +0900

    Fix RouteTable

--- lib/module/networkLayer/routeTable.ts
+++ lib/module/networkLayer/routeTable.ts
@@ -2,6 +2,7 @@ import { Construct } from 'constructs';
 import { Vpc, InternetGateway, RouteTable, RouteTableAssociation, Subnet } from '../../../.gen/providers/aws';
 
 export namespace RouteTableModule {
+  /** 不要になったら消す */
   export function create(scope: Construct, vpc: Vpc, internetGateway: InternetGateway): RouteTable {
     return new RouteTable(scope, 'route-for-cdktf', {
       vpcId: vpc.id,
@@ -31,6 +32,52 @@ export namespace RouteTableModule {
     return new RouteTableAssociation(scope, id, {
       routeTableId: routeTable.id,
       subnetId:     subnet.id
+    });
+  }
+
+  /**
+   * Public Route Table
+   */
+  export function createPublic(scope: Construct, vpc: Vpc, internetGateway: InternetGateway): RouteTable {
+    return new RouteTable(scope, 'rtb-public', {
+      vpcId: vpc.id,
+      route: [{
+        cidrBlock:              '0.0.0.0/0',
+        gatewayId:              internetGateway.id,
+        ipv6CidrBlock:          '',
+        egressOnlyGatewayId:    '',
+        instanceId:             '',
+        natGatewayId:           '',
+        networkInterfaceId:     '',
+        transitGatewayId:       '',
+        vpcPeeringConnectionId: ''
+      }],
+      tags: {
+        'Name': 'rtb-public'
+      }
+    });
+  }
+
+  /**
+   * Private Route Table
+   */
+  export function createPrivate(scope: Construct, vpc: Vpc): RouteTable {
+    return new RouteTable(scope, 'rtb-private', {
+      vpcId: vpc.id,
+      route: [{
+        cidrBlock:              '0.0.0.0/0',
+        gatewayId:              '',
+        ipv6CidrBlock:          '',
+        egressOnlyGatewayId:    '',
+        instanceId:             '',
+        natGatewayId:           '',
+        networkInterfaceId:     '',
+        transitGatewayId:       '',
+        vpcPeeringConnectionId: ''
+      }],
+      tags: {
+        'Name': 'rtb-private'
+      }
     });
   }
 }
