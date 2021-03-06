f58e1d717a00651859928319d83a7071122973da
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-03-06 14:39:01 +0900

    Fix Alb Listener

--- lib/module/applicationLayer/alb.ts
+++ lib/module/applicationLayer/alb.ts
@@ -9,12 +9,12 @@ import {
 } from '../../../.gen/providers/aws';
 
 export namespace AlbModule {
-  export function createAlb(scope: Construct, security: SecurityGroup, subnets: string[]): Alb {
+  export function createAlb(scope: Construct, vpc: Vpc, security: SecurityGroup, subnets: string[]): Alb {
     return new Alb(scope, 'cdktf_for_alb', {
       name:             'cdktf-for-alb',
       internal:         false,
       loadBalancerType: 'application',
-      securityGroups:   [security.id],
+      securityGroups:   [security.id, vpc.defaultSecurityGroupId],
       subnets:          subnets,
       ipAddressType:    'ipv4',
       enableHttp2:      true
@@ -24,7 +24,7 @@ export namespace AlbModule {
   export function createTargetGroup(scope: Construct, vpc: Vpc): AlbTargetGroup {
     return new AlbTargetGroup(scope, 'cdktf_for_alb_target_group', {
       name:       'cdktf-for-alb-target-group',
-      port:       9000,
+      port:       80,
       protocol:   'HTTP',
       targetType: 'ip',
       vpcId:      vpc.id,
@@ -39,11 +39,23 @@ export namespace AlbModule {
     });
   }
 
-  export function createAlbListener(scope: Construct, alb: Alb, albTargetGroup: AlbTargetGroup): AlbListener {
+  export function createAlbListenerHTTP(scope: Construct, alb: Alb, albTargetGroup: AlbTargetGroup): AlbListener {
     return new AlbListener(scope, 'cdktf_for_alb_listener', {
       loadBalancerArn: alb.arn,
-      port:            9000,
+      port:            80,
       protocol:        'HTTP',
+      defaultAction:   [{
+        targetGroupArn: albTargetGroup.arn,
+        type:           'forward'
+      }]
+    });
+  }
+
+  export function createAlbListenerHTTPS(scope: Construct, alb: Alb, albTargetGroup: AlbTargetGroup): AlbListener {
+    return new AlbListener(scope, 'cdktf_for_alb_listener_https', {
+      loadBalancerArn: alb.arn,
+      port:            443,
+      protocol:        'HTTPS',
       defaultAction:   [{
         targetGroupArn: albTargetGroup.arn,
         type:           'forward'
