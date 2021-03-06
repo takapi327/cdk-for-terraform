306f7ddd8d4eb0eecc50766cf00fe4d6f347bf46
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-03-06 16:52:57 +0900

    Delete unused

--- lib/module/networkLayer/subnet.ts
+++ lib/module/networkLayer/subnet.ts
@@ -2,25 +2,6 @@ import { Construct }   from 'constructs';
 import { Vpc, Subnet } from '../../../.gen/providers/aws';
 
 export namespace SubnetModule {
-  export function create1(scope: Construct, vpc: Vpc): Subnet {
-    return new Subnet(scope, 'subnet-for-cdktf', {
-      vpcId:               vpc.id,
-      availabilityZone:    'ap-northeast-1a',
-      cidrBlock:           '10.0.0.0/24',
-      mapPublicIpOnLaunch: true,
-      tags:                { ['Name']: 'ECS subnet-for-cdktf Public Subnet1' }
-    });
-  }
-
-  export function create2(scope: Construct, vpc: Vpc): Subnet {
-    return new Subnet(scope, 'subnet-for-cdktf2', {
-      vpcId:               vpc.id,
-      availabilityZone:    'ap-northeast-1c',
-      cidrBlock:           '10.0.1.0/24',
-      mapPublicIpOnLaunch: true,
-      tags:                { ['Name']: 'ECS subnet-for-cdktf Public Subnet2' }
-    });
-  }
 
   /**
    * Public Subnet
